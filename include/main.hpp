c2e52b3fa870899d33198b0c33d366aa2c715c7e
Noah Cohn <noahc42@gmail.com>
2023-02-20 18:25:14 -0800

    Update main.hpp

--- include/main.hpp
+++ include/main.hpp
@@ -2,6 +2,6 @@
 #include <cstdlib>
 #include <ctime>
 #include <emscripten.h>
-#undef RAND_MAX
+// #undef RAND_MAX
 // #define RAND_MAX (38D7EA4C68000) // quadrillion
 #define RAND_MAX (64)  //  hundred
